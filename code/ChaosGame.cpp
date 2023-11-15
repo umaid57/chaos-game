23d1c0f08d850147fd20170e6f59bf68912a84e0
umaid <leafshade56@gmail.com>
2023-11-14 22:35:15 -0800

    Elijah & Umaid

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -8,11 +8,25 @@
 // Make code easier to type with "using namespace"
 using namespace sf;
 using namespace std;
-void add_dot(vector<Vector2f>& points, vector<Vector2f> vertices) {
+void addpointTriangle(vector<Vector2f>& points, vector<Vector2f> vertices) {
     int randVert = rand() % 3;
     int dotX = (points[points.size() - 1].x + vertices[randVert].x) / 2;
     int dotY = (points[points.size() - 1].y + vertices[randVert].y) / 2;
-    points.push_back(Vector2f(dotX, dotY));
+    Vector2f point(dotX, dotY);
+
+    points.push_back(point);
+}
+void addpointSquare(vector<Vector2f>& points, vector<Vector2f> vertices, int* prevVert) {
+    int randVert = rand() % 4;
+    while (randVert == *prevVert) {
+        randVert = rand() % 4;
+    }
+    *prevVert = randVert;
+    int dotX = (points[points.size() - 1].x + vertices[randVert+1].x) / 2;
+    int dotY = (points[points.size() - 1].y + vertices[randVert+1].y) / 2;
+    Vector2f point(dotX, dotY);
+
+    points.push_back(point);   
 }
 int main()
 {
@@ -34,9 +48,8 @@ int main()
     font.loadFromFile("fonts/Octosquares.ttf");
 
     sf::Text text;
+    sf::Text text1;
     text.setFont(font);
-    // set the string to display
-    text.setString("Click at three different points to form a triangle!");
 
     // set the character size
     text.setCharacterSize(50); // in pixels, not points!
@@ -44,13 +57,19 @@ int main()
     // set the color
     text.setFillColor(sf::Color::White);
     
-
+    text1 = text;
+    // set the string to display
+    text.setString("Click at three different points to form a triangle!");
 
     // set the text style
    // text.setStyle(sf::Text::Bold | sf::Text::Underlined);
     text.setPosition(500, 0);
     
+    int* prevVert = new int;
+    *prevVert = rand() % 4;
 
+    
+    bool isSquare = false;
 
 	while (window.isOpen())
 	{
@@ -74,23 +93,34 @@ int main()
                     std::cout << "the left button was pressed" << std::endl;
                     std::cout << "mouse x: " << event.mouseButton.x << std::endl;
                     std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-
-                    if(vertices.size() < 3)
+                    if(event.mouseButton.x < 200 && vertices.size() == 0){
+                        isSquare = true;
+                        
+                    }
+                    if(vertices.size() < 5 && isSquare)
                     {
                         vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
                     }
-                    else if(points.size() == 0)
+                    else if(points.size() == 0 && vertices.size() >= 3)
                     {
                         
                         points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
                         ///fourth click
                         ///push back to points vector
                     }
+                    else {
+                        vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+                    }
                 }
             }
         }
+
+        if (isSquare) {
+            text.setString("Click at 4 points to form a square");
+
+        }
         
-        if (vertices.size() == 3 && points.size() == 0) {
+        if ((vertices.size() == 3 && !isSquare) || (vertices.size() == 5 && isSquare)) {
             text.setString("Click anywhere on screen to begin!");
         }
         if (Keyboard::isKeyPressed(Keyboard::Escape))
@@ -105,12 +135,25 @@ int main()
                 
         if(points.size() > 0 && points.size() < 10000)
         {
-            text.setString("The Sierpinski triangle!");
-            add_dot(points, vertices);
-            add_dot(points, vertices);
-            add_dot(points, vertices);
-            add_dot(points, vertices);
-            add_dot(points, vertices);
+            
+            if (isSquare) {
+                text.setString("Square fractal!");
+                addpointSquare(points, vertices, prevVert);
+                addpointSquare(points, vertices, prevVert);
+                addpointSquare(points, vertices, prevVert);
+                addpointSquare(points, vertices, prevVert);
+                addpointSquare(points, vertices, prevVert);
+            }
+            else {
+                text.setString("The Sierpinski triangle!");
+                addpointTriangle(points, vertices);
+                addpointTriangle(points, vertices);
+                addpointTriangle(points, vertices);
+                addpointTriangle(points, vertices);
+                addpointTriangle(points, vertices);
+            }
+
+
             ///generate more point(s)
             ///select random vertex
             ///calculate midpoint between random vertex and the last point in the vector
@@ -126,8 +169,16 @@ int main()
         window.clear();
 
         window.draw(spriteBackground);
-        // inside the main loop, between window.clear() and window.display()
         window.draw(text);
+
+        text1.setString("Click the square to generate a square fractal");
+        text1.setPosition(Vector2f(0, 100));
+        window.draw(text1);
+        
+        RectangleShape rectShape(Vector2f(50, 50));
+        rectShape.setPosition(Vector2f(0, 200));
+        rectShape.setFillColor(Color::Magenta);
+        window.draw(rectShape);
 
         for(int i = 0; i < vertices.size(); i++)
         {
@@ -138,7 +189,7 @@ int main()
         }
 
         for (int i = 0; i < points.size(); i++) {
-            CircleShape dot(1);
+            CircleShape dot(2);
             dot.setPosition(Vector2f(points[i].x, points[i].y));
             dot.setFillColor(Color::Yellow);
             window.draw(dot);
@@ -146,4 +197,5 @@ int main()
 
         window.display();
     }
+    delete prevVert;
 }
